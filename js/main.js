5a04b5bc1bec0e070af4a543d4c5b7aa973519e3
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-07-31 18:42:43 -0700

    add: footer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function () {
+
+  $('#header').height($(window).height());
+
   setTimeout(function () {
     $('#hand').addClass('in');
   }, 1000);
@@ -6,4 +9,14 @@ $(function () {
   setTimeout(function () {
     $('#text').addClass('in');
   }, 1000);
+
+  $('#columns').waypoint(function () {
+    $('.column').eq(1).addClass('in');
+    setTimeout(function () {
+      $('.column').eq(0).addClass('in');
+      $('.column').eq(2).addClass('in');
+    }, 500);
+  }, {
+    offset: 500
+  });
 });
